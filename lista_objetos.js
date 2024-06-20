8b7083233119a697e2c642e710f5aa2d4c083716
luquinhas10 <162441126+luquinhas10@users.noreply.github.com>
2024-06-20 08:36:43 -0300

    Add files via upload

---
+++ lista_objetos.js
@@ -0,0 +1,13 @@
+const cliente = {
+    nome :"João",
+    idade: 24,
+    email:"joao@larissa.com",
+    telefone:["(41) 9999-0000","(41) 9988-0000"],
+};
+
+cliente.endereco = {
+    rua : "R. Joseph Climber",
+    numero : 1337,
+    apartamento : true,
+    complemento : "ap 934",                
+};
